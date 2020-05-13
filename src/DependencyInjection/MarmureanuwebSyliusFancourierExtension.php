ec89f926242c44ae1ba3d35a3c9e1e2ea1ce1504
Andrei Marmureanu <a.marmureanu@fitforme.nl>
2020-05-14 00:19:52 +0300

    [MASTER] - first commit; added template of sylius plugin and modified the simple files

---
+++ src/DependencyInjection/MarmureanuwebSyliusFancourierExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file has been created by developers from Marmureanur.ro.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://marmureanu.ro and write us
+ * an email on marmureanuweb@gmail.com
+ */
+
+namespace Marmureanuweb\SyliusFancourierPlugin\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+final class MarmureanuwebSyliusFancourierExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container): void
+    {
+    }
+}
---
+++ src/MarmureanuwebSyliusFancourierPlugin.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file has been created by developers from Marmureanur.ro.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://marmureanu.ro and write us
+ * an email on marmureanuweb@gmail.com
+ */
+
+namespace Marmureanuweb\SyliusFancourierPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MarmureanuwebSyliusFancourierPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
